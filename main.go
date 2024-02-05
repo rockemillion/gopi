78da7a9056484636feee4367aecbf26290bde12a
Luiz M <148032407+rockemillion@users.noreply.github.com>
2024-02-04 23:20:23 -0300

    Add files via upload

---
+++ main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+)
+
+func seedAccount(store Storage, fname, lname, pw string) *Account {
+	acc, err := NewAccount(fname, lname, pw)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := store.CreateAccount(acc); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("new account => ", acc.Number)
+
+	return acc
+}
+
+func seedAccounts(s Storage) {
+	defaultPass := os.Getenv("DEFAULT_PASS")
+	seedAccount(s, "admin", "1", defaultPass)
+}
+
+func main() {
+	seed := flag.Bool("seed", false, "seed the db")
+	flag.Parse()
+
+	store, err := NewPostgressStore()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := store.Init(); err != nil {
+		log.Fatal(err)
+	}
+
+	if *seed {
+		fmt.Println("seeding the database")
+		seedAccounts(store)
+	}
+
+	server := NewApiServer(":3000", store)
+	server.Run()
+}
